1fb4d0991043c6673c6790f20363eb34bd28f6d8
Ali-Coding123 <alibuzcu94@gmail.com>
2025-07-26 00:49:40 +0300

    Update datas.js

--- datas.js
+++ datas.js
@@ -1,15 +1,15 @@
 const articlesData = [
+  {title: "Divan kartı açılmadı",
+   author: "Kutay (4. Dönem Padişahı)",
+   date: "2025-07-25",
+   basicInfo : "Kartın Türkiye'deki birtakım problemler yüzünden açılmadı. Bunun sebebi paparanın oluşturduğu sorunlardı. Kart henüz açıklamayacak",
+   inside: "Kart belirli bir açıklama gelene kadar açılmayacak. Sebebi ise marketlerden çıkarılmaması. Yani ertelendi veya kaldırıldı. İmza: dönem padişahlığı"
+  },
   {
     title: "Haber Sitesi Açıldı!",
     author: "Ali Yılmaz Buzcu (4. Dönem Padişahı)",
     date: "2025-07-24",
     basicInfo: "Yeni sitemiz hayırlı uğurlu olsun!",
     inside: "Artık Divan-i Erkek-i'de yaşanan güncel haberleri alabilirsiniz! "
-  },
-  {title: "Divan kartı açılmadı",
-   author: "Kutay (4. Dönem Padişahı)",
-   date: "2025-07-25",
-   basicInfo : "Kartın Türkiye'deki birtakım problemler yüzünden açılmadı. Bunun sebebi paparanın oluşturduğu sorunlardı. Kart henüz açıklamayacak",
-   inside: "Kart belirli bir açıklama gelene kadar açılmayacak. Sebebi ise marketlerden çıkarılmaması. Yani ertelendi veya kaldırıldı. İmza: dönem padişahlığı"
   }
 ];
